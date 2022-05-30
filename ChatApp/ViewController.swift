d2888263465244faae9ab3838c529c7ea5ed1da1
Gary <gary.odonoghue@themobilecompany.com>
2022-05-30 21:20:08 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by gary.odonoghue  on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
